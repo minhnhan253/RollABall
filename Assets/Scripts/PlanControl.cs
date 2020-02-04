a185c4003136d25d466cf4e7ccf202d1d89f2217
minhnhan253 <nminhnhan@gmail.com>
2020-02-04 11:58:24 +0700

    added item when click

---
+++ Assets/Scripts/PlanControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+ 
+            RaycastHit hit = new RaycastHit();
+    
+            if (Physics.Raycast (ray, out hit))
+            {         
+                Vector3  newPos = new Vector3(hit.point.x, 0.5f, hit.point.z);
+                Instantiate(myObject, newPos, myObject.transform.rotation);
+            }
+        }
+    }
+    
+}
