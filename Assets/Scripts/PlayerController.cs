07a7858ae1ee441a299e773aa834809df0d63e2d
minhnhan253 <nminhnhan@gmail.com>
2020-02-04 11:25:33 +0700

    added count, win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    private int count;
+    public Text countText;
+    public Text winText;
     private Rigidbody rb;
     public float speed;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
    void Update(){
 
@@ -24,7 +31,15 @@ public class PlayerController : MonoBehaviour
       if(other.gameObject.CompareTag("Pick Up"))
       {
           other.gameObject.SetActive(false);
+          count ++;
+          SetCountText();
       }
 
+   }
+   void SetCountText()
+   {
+       countText.text = "Count: " + count.ToString();
+       if (count >= 12)
+        winText.text = "You Win";
    }
 }
