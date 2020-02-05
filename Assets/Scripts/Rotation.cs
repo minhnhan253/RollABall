be0638107728fa6ab44fe322b9868396d74e349d
minhnhan253 <nminhnhan@gmail.com>
2020-02-05 18:04:42 +0700

    anim WIP

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-
+    private Animator anim;
+    private List<string> animList;
+    void Start() {
+        animList.Add("prefab_blue");
+        animList.Add("prefab_green");
+        animList.Add("prefab_red");
+        anim = GetComponent<Animator>();
+        anim.Play("prefab_blue");
+        Debug.Log("calll!!!!!!!!!!!!!!!!");
+    }
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        if (!AnimatorIsPlaying())
+        {
+            anim.Play("prefab_green");
+        }
     }
+
+    bool AnimatorIsPlaying(){
+     return anim.GetCurrentAnimatorStateInfo(0).length >
+            anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
+  }
 }
