abf3f1ae5fc3962dfc7553bd82c7e72fdb757a78
minhnhan253 <nminhnhan@gmail.com>
2020-02-06 09:38:53 +0700

    added array

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     private Animator anim;
-    private List<string> animList;
+    private string[] animList;
+    private int currAnimId = 0;
     void Start() {
-        animList.Add("prefab_blue");
-        animList.Add("prefab_green");
-        animList.Add("prefab_red");
+        currAnimId = 0;
+        animList = new string[3];
+        animList.Initialize();
+        animList.SetValue("prefab_blue", 0);
+        animList.SetValue("prefab_green",1);
+        animList.SetValue("prefab_red", 2);
         anim = GetComponent<Animator>();
-        anim.Play("prefab_blue");
-        Debug.Log("calll!!!!!!!!!!!!!!!!");
+        anim.Play(animList.GetValue(currAnimId).ToString());
     }
     // Update is called once per frame
     void Update()
     {
         if (!AnimatorIsPlaying())
         {
-            anim.Play("prefab_green");
+            SetAnim();
         }
     }
 
     bool AnimatorIsPlaying(){
      return anim.GetCurrentAnimatorStateInfo(0).length >
             anim.GetCurrentAnimatorStateInfo(0).normalizedTime;
-  }
+    }
+    void SetAnim()
+    {
+        currAnimId =  (currAnimId >= animList.Length - 1) ? 0 : currAnimId+1;
+        anim.Play(animList.GetValue(currAnimId).ToString());
+    }
 }
