c7f8beb631fcb8dfb8bcac00d6387d57c813e5ec
minhnhan253 <nminhnhan@gmail.com>
2020-02-10 10:28:14 +0700

    cleaned code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -48,30 +48,8 @@ public class PlayerController : MonoBehaviour
       if(other.gameObject.CompareTag("Pick Up"))
       {
         itemManager.listItem.Remove(other.gameObject);
-        
-        // Animator anim = other.gameObject.GetComponent<Animator>();
-
-        // if (anim.GetCurrentAnimatorStateInfo(0).IsName("prefab_red"))
-        // {
-        //     GetComponent<Renderer>().material.color = new Color(1, 0, 0, 1);
-        //     Debug.Log("red");
-        // }
-        // else
-        // if (anim.GetCurrentAnimatorStateInfo(0).IsName("prefab_green"))
-        // {
-        //     Debug.Log("green");
-        //     GetComponent<Renderer>().material.color = new Color(0, 1, 0, 1);
-        // }
-            
-        // else
-        // if (anim.GetCurrentAnimatorStateInfo(0).IsName("prefab_blue"))
-        // {
-        //     Debug.Log("blue");
-        //     GetComponent<Renderer>().material.color = new Color(0, 0, 1, 1);
-        // }
-            
-          count ++;
-          SetCountText();
+        count ++;
+        SetCountText();
         other.gameObject.Kill();
       }
 
