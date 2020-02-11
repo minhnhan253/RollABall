7c29f63c7d2f88e26e667b14cf9f16f44c434759
minhnhan253 <nminhnhan@gmail.com>
2020-02-11 11:51:46 +0700

    update moving camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Vector3 offset;
     private List<Camera> list;
-    private int currIdx = 0;
     public Camera camera1;
     public Camera camera2;
+    public Camera movingCamera;
     // Start is called before the first frame update
+    private float time = 0;
     void Start()
     {
-        //offset = transform.position - player.transform.position;
-        // list.Add(camera1);
-        // list.Add(camera2);
-        camera1.enabled = true;
+        camera1.enabled = false;
+        camera2.enabled = false;
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-       // transform.position = player.transform.position + offset;
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            if (camera1.isActiveAndEnabled)
-            {
-                camera1.enabled = false;
-                camera2.enabled = true;
-            }
-            else
-            {
-                camera1.enabled = true;
-                camera2.enabled = false;
-            }
+        time += 0.1f*Time.deltaTime;
+        //if (Input.GetKeyUp(KeyCode.Space))
+        {   
+            movingCamera.transform.position = Vector3.Lerp(camera1.transform.position, camera2.transform.position, time);
+            movingCamera.transform.rotation = Quaternion.Lerp(camera1.transform.rotation, camera2.transform.rotation, time);
+            
         }
     }
 }
---
+++ Assets/Scripts/MovingPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlayer : MonoBehaviour
+{
+    public GameObject fromObj;
+    public GameObject targetObj;
+    [Range(0, 1)] public float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(fromObj.transform.position, targetObj.transform.position, t);
+        transform.rotation = Quaternion.Lerp(fromObj.transform.rotation, targetObj.transform.rotation, t);
+    }
+}
