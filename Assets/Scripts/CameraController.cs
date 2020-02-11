b9f71be70924e590b5083b0941a6d913cbe86f02
minhnhan253 <nminhnhan@gmail.com>
2020-02-11 10:57:28 +0700

    added 2 cameras

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
     private Vector3 offset;
+    private List<Camera> list;
+    private int currIdx = 0;
+    public Camera camera1;
+    public Camera camera2;
     // Start is called before the first frame update
     void Start()
     {
         //offset = transform.position - player.transform.position;
+        // list.Add(camera1);
+        // list.Add(camera2);
+        camera1.enabled = true;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
        // transform.position = player.transform.position + offset;
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (camera1.isActiveAndEnabled)
+            {
+                camera1.enabled = false;
+                camera2.enabled = true;
+            }
+            else
+            {
+                camera1.enabled = true;
+                camera2.enabled = false;
+            }
+        }
     }
 }
